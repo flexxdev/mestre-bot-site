7fc5850733224745226de2378bd282229099a493
flexxdev <deyvisomdanilo61@gmail.com>
2025-02-05 01:34:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("comprarAgora").addEventListener("click", function() {
+    const linkDePagamento = "https://pay.kiwify.com.br/eDIPQiX"; // Substitua pelo seu link real
+    window.location.href = linkDePagamento;
+});
